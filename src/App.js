3ab786c2d9ceaa22ca69959952c05f5b923916c9
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-04-18 14:26:18 +0100

    Made initial set ups by creating the component folder and the pages folder to keep the web pages in one place.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import NavBar from './Components/NavBar'
+import NotFound from './Pages/NotFound'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
