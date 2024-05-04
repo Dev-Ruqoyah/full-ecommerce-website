8ee0f0b214c562d2bb263ce95df9178d4e47b71d
bossLady0001 <hamzatrukayatabiodun02@gmail.com>
2024-05-04 07:39:54 -0700

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,123 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className="bg-black text-white mt-5">
+        <div className="container mx-auto py-8">
+          <div className="flex justify-evenly gap-6  ">
+            <div className="exclusive flex flex-col gap-2 basis-1/2 md:basis-1/5">
+              <h2 className="text-xl font-semibold">Exclusive</h2>
+              <p className="font-semibold">Subscribe</p>
+              <p>Get 10% off your first order</p>
+              <div className="flex justify-between items-center px-2 border ">
+                <input
+                  type="email"
+                  className="border-0 bg-transparent"
+                  name=""
+                  id=""
+                  placeholder="Enter your email"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6 text-white"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="support flex flex-col gap-2 basis-1/2 md:basis-1/5">
+              <h4 className="font-semibold text-xl">Support</h4>
+              <ul className="flex flex-col gap-1">
+                <li>
+                  <a href="#">
+                    111 Bijay sarani,Dhaka,
+                    <br />
+                    DH1515,Bangladesh
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:exclusive@gmail.com">exclusive@gmail.com</a>
+                </li>
+                <li>
+                  <a href="tel:+88015-88888-9999">+88015-88888-9999</a>
+                </li>
+              </ul>
+            </div>
+            <div className="support flex flex-col gap-2 basis-1/2 md:basis-1/5">
+              <h4 className="font-semibold text-xl">Account</h4>
+              <ul className="flex flex-col gap-1">
+                <li>
+                  <a href="#">My account</a>
+                </li>
+                <li>
+                  <a href="#">Login/Register</a>
+                </li>
+                <li>
+                  <a href="#">Cart</a>
+                </li>
+                <li>
+                  <a href="#">Wishlist</a>
+                </li>
+                <li>
+                  <a href="#">Shop</a>
+                </li>
+              </ul>
+            </div>
+            <div className="support flex flex-col gap-2 basis-1/2 md:basis-1/5">
+              <h4 className="font-semibold text-xl">Quick Link</h4>
+              <ul className="flex flex-col gap-1">
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Term of Use</a>
+                </li>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="support flex flex-col gap-2 basis-1/2 md:basis-1/5">
+              <h4 className="font-semibold text-xl">Download App</h4>
+              <p>Save $3 with App Now user Only</p>
+              <div className="qrcode">
+                <div className="grid grid-cols-2 gap-2">
+                  <div className="first w-full ">
+                    <img src="qrcode.png" width={'90%'} alt="" />
+                  </div>
+                  <div className="second flex flex-col gap-2">
+                    <img src="playstore.png" alt="" />
+                    <img src="appstore.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className="social-links flex gap-3 justify-between py-3">
+                  <img src="f.png" alt="" />
+                  <img src="twitter.png" alt="" />
+                  <img src="insta.png" alt="" />
+                  <img src="linkedin.png" alt="" />
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        <div className="border-b"></div>
+        <p className="text-center p-5">@Copyright Rimel 2022 All right reserved</p>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2422,24 +2422,64 @@ const HomePage = () => {
             </div>
             <div className="fast flex-col items-center justify-center mx-auto">
               <div className="bg-gray-300 w-12 h-12 rounded-full p-2 mx-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 p-1 bg-black rounded-full text-white">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
-</svg>
-
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-8 h-8 p-1 bg-black rounded-full text-white"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+                  />
+                </svg>
               </div>
               <h3 className="text-xl font-semibold">24/7 CUSTOMER SERVICE</h3>
               <p>Friendly 24/7 customer service</p>
             </div>
             <div className="fast flex-col items-center justify-center mx-auto">
               <div className="bg-gray-300 w-12 h-12 rounded-full p-2 mx-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 p-1 bg-black text-white rounded-full">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-</svg>
-
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-8 h-8 p-1 bg-black text-white rounded-full"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
               </div>
               <h3 className="text-xl font-semibold">MONEY BACK GUARANTEE</h3>
               <p>We return money in 30 days</p>
             </div>
+          </div>
+          <div className="top-down-button flex justify-end ">
+            <button className="p-3 rounded-full bg-gray-300">
+              <a href="#">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-8 h-8"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
+                  />
+                </svg>
+              </a>
+            </button>
           </div>
         </div>
       </section>
